4609e658c8cf4f88db979b38161bacefe54fe001
huylesitdn <mrsjiodev@gmail.com>
2022-05-28 10:09:26 +0700

    menu

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -34,7 +34,7 @@ var translator = new Translator({
 const PREFERED_REGION = 'preferred_region';
 const _get_translator_config = translator.config.persistKey || "preferred_language";
 const _get_language = localStorage.getItem(_get_translator_config) || LANGUAGES.EN;
-const _get_region = localStorage.getItem(PREFERED_REGION, 'Singapore');
+const _get_region = localStorage.getItem(PREFERED_REGION) || 'Singapore';
 
 translator.fetch([LANGUAGES.EN, LANGUAGES.ZH]).then(() => {
   // -> Translations are ready...
